d561cf0a4cb646288550383e1cdbaa07a3c95d8b
Cameron Burrows <cameronb@outlook.com.au>
2021-09-24 16:49:39 +1000

    added search test, doesn't work yet tho

---
+++ src/youtube/search.test.ts
@@ -0,0 +1,8 @@
+import { youtubeSearch } from './search'
+
+// describe('Youtube Search', () => {
+//   test('should get youtube search results', async () => {
+//     const result = await youtubeSearch('Never gonna give you up')
+//     expect(result).not.toBeNull
+//   })
+// })
