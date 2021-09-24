b1556172d8563e75d1af6df89dddae686c61a7e1
Cameron Burrows <cameronb@outlook.com.au>
2021-09-24 16:53:09 +1000

    added placeholder test

--- src/youtube/search.test.ts
+++ src/youtube/search.test.ts
@@ -1,8 +1,11 @@
 import { youtubeSearch } from './search'
 
-// describe('Youtube Search', () => {
-//   test('should get youtube search results', async () => {
-//     const result = await youtubeSearch('Never gonna give you up')
-//     expect(result).not.toBeNull
-//   })
-// })
+describe('Youtube Search', () => {
+  // test('should get youtube search results', async () => {
+  //   const result = await youtubeSearch('Never gonna give you up')
+  //   expect(result).not.toBeNull
+  // })
+  test('should be placeholder test', () => {
+    expect(1 + 1).toBe(2) // god i hope so
+  })
+})
